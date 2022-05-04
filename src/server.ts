28dda2a9ae80551eb85d16d90f5ca9ce5539267c
LucasdaCostaSilva <landi_cnt@yahoo.com>
2022-05-04 09:03:57 -0300

    prisma @prisma/client

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import express from 'express'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.header('location', 'http://localhost:3333/feedback')
+  res.status(302).send('Hello World!')
+})
+
+app.get('/feedback', (req, res) => {
+  res.send('Feedback received' + JSON.stringify(req.body))
+})
+
 app.listen(3333, () => {
   console.log('Server started on port 3333!')
 })
