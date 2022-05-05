9972ba796364f63ce8ec8a9236bca9774e762da2
LucasdaCostaSilva <landi_cnt@yahoo.com>
2022-05-05 09:18:00 -0300

    phosphor-react-native react-native-iphone-x-helper expo install react-native-svg

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
-import { styles } from "./styles";
+import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native';
+import { styles } from './styles';
+import { theme } from '../../theme';
 
 export function Widget() {
   return (
-    <View style={styles.container}>
-      <Text>oi</Text>
-    </View>
+    <>
+      <TouchableOpacity style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          color={theme.colors.text_on_brand_color} />
+      </TouchableOpacity>
+    </>
   );
 }
