1d7c0196f1b8161e51bc2911471804ef9c45bffd
LucasdaCostaSilva <landi_cnt@yahoo.com>
2022-05-05 08:55:26 -0300

    expo install  expo-font @expo-google-fonts/inter expo-app-loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
+import { Inter_400Regular, Inter_500Medium, useFonts } from '@expo-google-fonts/inter';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Widget } from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Widget Feedback!</Text>
-      <StatusBar style="auto" />
+      <Widget />
+      <StatusBar
+        style="light"
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }
---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Widget() {
+  return (
+    <View style={styles.container}>
+      <Text>oi</Text>
+    </View>
+  );
+}
