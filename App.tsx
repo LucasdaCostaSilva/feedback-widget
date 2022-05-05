39887843566d076e5aa22fb9f8fcbf5344fa9121
LucasdaCostaSilva <landi_cnt@yahoo.com>
2022-05-05 14:30:32 -0300

    expo install @gorhom/bottom-sheet@^4 expo install react-native-reanimated react-native-gesture-handler

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import { Inter_400Regular, Inter_500Medium, useFonts } from '@expo-google-fonts/inter';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
@@ -17,12 +19,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Widget Feedback!</Text>
-      <Widget />
       <StatusBar
         style="light"
         backgroundColor="transparent"
         translucent
       />
+      <Widget />
     </View>
   );
 }
