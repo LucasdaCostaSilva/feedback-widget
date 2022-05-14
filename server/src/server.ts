3c07d840fc72c5791387e74b960f6e0811ce052e
LucasdaCostaSilva <landi_cnt@yahoo.com>
2022-05-14 09:56:18 -0300

    with heroku config

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,13 @@ import express from 'express'
 import { routes } from './rotas'
 import cors from 'cors'
 
+const PORT = process.env.PORT || 3333
 const app = express()
 
 app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(routes)
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!')
+app.listen(PORT, () => {
+  console.log('Server started on port ${PORT}!')
 })
